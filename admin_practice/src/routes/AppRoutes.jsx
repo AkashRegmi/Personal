1b4a05bb2a28dfb9f6baa694c35d2018a643c98a
Akash Regmi <akashregmi82@gmail.com>
2026-08-24 19:01:58 +0545

    Completed the logout model and feature

--- admin_practice/src/routes/AppRoutes.jsx
+++ admin_practice/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import ProductsPage from "../pages/ProductsPage";
 import OrdersPage from "../pages/OrdersPage";
 import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -12,11 +13,15 @@ const AppRoutes = () => {
       {/* Public route - does NOT use MainLayout */}
       <Route path="/login" element={<LoginPage />} />
       {/* Routes  that follow  WITH MainLayout */}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/orders" element={<OrdersPage />} />
+      <Route element={<ProtectedRoute />}>
+        {/* Routes  that follow  WITH MainLayout */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+        </Route>
       </Route>
+
       {/* 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
