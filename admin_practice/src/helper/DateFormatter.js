5dc5bc966022bb65bbb83d874971bed138b1b765
Akash Regmi <akashregmi82@gmail.com>
2026-09-18 14:54:18 +0545

    Remove old admin_practice folder

--- admin_practice/src/helper/DateFormatter.js
+++
@@ -1,4 +0,0 @@
-export const extractDate = (date) => {
-  const formattedDat = date.split("T")[0];
-  return formattedDat;
-};
--- admin_practice/src/hooks/useDashboard.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { dashboardService } from "../services/dashboard.services";
-
-export const useDashboard = () => {
-  return useQuery({
-    queryKey: ["dashboard"],
-    queryFn: dashboardService,
-  });
-};
